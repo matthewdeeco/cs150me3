22c0e2710453467cb60210de10fd5e5a4432d0ab
Matthew <matthew@maxwell.thornbow>
2013-07-23 21:30:15 +0800

    First commit

---
+++ me3.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+public class Main {
+	private String input;
+	private String[][] arr;
+	
+	private Stack<Integer> s;
+	
+	public static void main(String[] arg){
+		Main obj=new Main(arg);
+		
+	}
+	
+	public Main(String arr){
+		this.input=arr[0];
+		s=new Stack<Integer>();
+		arr=new String[10][8];
+		
+		
+		
+	
+	}
+	
+	private void createTable(){
+		for(int i=0; i<arr.length; i++){
+			for(int j=0; j<arr[i].length; j++){
+				arr[i][j]="err";
+			
+			}
+		}
+		arr[1][0]="S6";
+		arr[2][0]="R2";
+		arr[3][0]="R4";
+		arr[4][0]="R6";
+		arr[5][0]="R7";
+		arr[7][0]="";
+		
+		arr = {
+		{}, {}, 
+		};
+		
+	
+	
+	
+	}
+	private void retrieveNextOne(){
+		
+		String tmp=
+		while(tmp!=){
+		
+		}
+	}
+	private void PushToStack(){
+	
+	
+	}
+	
+
+
+}
